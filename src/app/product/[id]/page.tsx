1c9c61730167bc03a2e32b5d0e5dc483fecd973d
Fallen <habibnaufal621@gmail.com>
2025-07-07 07:51:07 +0700

    update dinamic url routing

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,7 +2,7 @@ export default async function Product({ params }: {
     params: Promise<{ id: string }>
 }) {
     const resultParams = await params;
-    console.log("idnya = ", resultParams.id);
+    console.log("idnya = g", resultParams.id);
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Detail Product Page</h1>
